f6213838168bb0351495ec9d4f9c0bc36df44316
tenmark86e <tenmark86@gmail.com>
2022-09-30 14:44:13 +0300

    Done Async Giphy Search Lab assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import GifListContainer from './GifListContainer'
 
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
-    <div>
+    <div >
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer/>
     </div>
   );
 }
